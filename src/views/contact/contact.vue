198b2883386b92fed07b2ce2bf16bd1bf13aa0eb
Furman1331 <michalfurmanczak@gmail.com>
2022-12-22 22:13:18 +0100

    Add license and move create contact page.

---
+++ src/views/contact/contact.vue
@@ -0,0 +1,10 @@
+<template lang="pug" src="./contact.pug" />
+<style lang="less" src="./contact.less" scoped />
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
+
